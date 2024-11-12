fba67279dcb2f8416a2dc64d457e0d64ac1d8031
Deniz Servet ALTUN <denizservetaltun@gmail.com>
2024-11-12 23:25:57 +0300

    Upload first game.

---
+++ Assets/Scripts/Baslangıc.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Baslangýc : MonoBehaviour
+{
+    public void Basla()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+    public void Ayarlar()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void Credits()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
